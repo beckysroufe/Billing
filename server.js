c15085a78c6c755295504b3ee5940f04998bc116
Faiz K <faizkhan00@gmail.com>
2014-03-19 20:08:06 -0500

    init server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    wine = require('./routes/wines');
+ 
+var app = express();
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+ 
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');

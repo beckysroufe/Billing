66abadeb0073c2e6f8d1a818bdf23c29c3d761fa
Faiz K <faizkhan00@gmail.com>
2014-03-20 12:23:03 -0500

    init commit 3 or 4

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var express = require('express'),
-    wine = require('./routes/wines');
+    bill = require('./controllers/bills.js');
  
 var app = express();
  
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
+    app.use(express.static(__dirname+'/public'));
 });
  
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+app.get('/bill', bill.findAll);
+app.get('/bills/:id', bill.findById);
+app.post('/bills', bill.addBill);
+app.put('/bills/:id', bill.updateBill);
+app.delete('/bills/:id', bill.deleteBill);
  
 app.listen(3000);
 console.log('Listening on port 3000...');

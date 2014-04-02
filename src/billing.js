dd0d86ee2ae631a219ba7e287aa60eb1586fea33
ianmstew <ianmstew@gmail.com>
2014-04-02 10:42:01 -0400

    Changed javascript format to airbnb (which is loosely based on Google)

--- src/billing.js
+++ src/billing.js
@@ -1,26 +1,26 @@
-define([ "marionette", "hgn!testTemplate" ],
-function( Marionette ,  testTemplate      ) {
+define(function(require) {
+  var Marionette = require('marionette'),
+      testTemplate = require('hgn!testTemplate'),
+      Billing = new Marionette.Application();
 
-    Marionette.Renderer.render = function( template, data ) {
-        return template( data );
-    };
+  Marionette.Renderer.render = function( template, data ) {
+    return template( data );
+  };
 
-    var Billing = new Marionette.Application();
+  Billing.addRegions({
+    headerRegion: '#header-region',
+    mainRegion: '#main-region',
+    footerRegion: '#footer-region'
+  });
 
-    Billing.addRegions({
-        headerRegion: "#header-region",
-        mainRegion: "#main-region",
-        footerRegion: "#footer-region"
-    });
+  Billing.TestView = Marionette.ItemView.extend({
+    template: testTemplate
+  });
 
-    Billing.TestView = Marionette.ItemView.extend({
-        template: testTemplate
-    });
+  Billing.on('initialize:after', function() {
+    var testView = new Billing.TestView();
+    Billing.mainRegion.show(testView);
+  });
 
-    Billing.on( "initialize:after", function () {
-        var testView = new Billing.TestView();
-        Billing.mainRegion.show( testView );
-    });
-
-    return Billing;
+  return Billing;
 });
